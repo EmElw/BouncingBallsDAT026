c7933ac2a60e51d579ce37682a361e794a1c53f4
Magnus Wamby <wamby@student.chalmers.se>
2018-04-26 12:45:11 +0200

    Initial commit

---
+++ src/Model.java
@@ -0,0 +1,79 @@
+/**
+ * The physics model.
+ * <p>
+ * This class is where you should implement your bouncing balls model.
+ * <p>
+ * The code has intentionally been kept as simple as possible, but if you wish, you can improve the design.
+ *
+ * @author Simon Robillard
+ */
+class Model {
+
+    private static final double GRAVITY = 0;
+    double areaWidth, areaHeight;
+
+    Ball[] balls;
+
+    Model(double width, double height) {
+        areaWidth = width;
+        areaHeight = height;
+
+        // Initialize the model with a few balls
+        balls = new Ball[2];
+        balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.2);
+        balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3);
+    }
+
+    void step(double deltaT) {
+        // TODO this method implements one step of simulation with a step deltaT
+        for (Ball b : balls) {
+            // detect collision with the border
+            if (b.x < b.radius || b.x > areaWidth - b.radius) {
+                b.vx *= -1; // change direction of ball
+            }
+            if (b.y < b.radius || b.y > areaHeight - b.radius) {
+                b.vy *= -1;
+            }
+
+            // compute new position according to the speed of the ball
+            b.x += deltaT * b.vx;
+            b.y += deltaT * b.vy;
+        }
+    }
+
+    /**
+     * Simple inner class describing balls.
+     */
+    class Ball {
+
+        Ball(double x, double y, double vx, double vy, double r) {
+            this.x = x;
+            this.y = y;
+            this.vx = vx;
+            this.vy = vy;
+            this.radius = r;
+        }
+
+        /**
+         * Position, speed, and radius of the ball. You may wish to add other attributes.
+         */
+        double x, y, vx, vy, radius;
+
+        public double potentialEnergy() {
+            return mass() * GRAVITY * y;
+        }
+
+        private double mass() {
+            return 4.0 / 3.0 * Math.PI * Math.pow(radius, 3);
+        }
+
+        public double kineticEnergy() {
+            return 1.0 / 2.0 * mass() * velocity();
+        }
+
+        private double velocity() {
+            return Math.sqrt(vx * vx + vy * vy);
+        }
+
+    }
+}

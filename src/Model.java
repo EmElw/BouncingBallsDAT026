b3455ccec66509065fa19fb9c7bb2f1cddd4c3af
Magnus Wamby <wamby@student.chalmers.se>
2018-05-09 16:54:14 +0200

    minor improvements

--- src/Model.java
+++ src/Model.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * The physics model.
  * <p>
@@ -9,37 +11,47 @@
  */
 class Model {
 
-    private static final double GRAVITY = 0.1;
+    static final double GRAVITY = 10;
     double areaWidth, areaHeight;
 
-    Ball[] balls;
+    ArrayList<Ball> balls;
 
     Model(double width, double height) {
         areaWidth = width;
         areaHeight = height;
 
         // Initialize the model with a few balls
-        balls = new Ball[2];
-        balls[0] = new Ball(width / 3, height * 0.9, 0, 0, 0.2);
-        balls[1] = new Ball(2 * width / 3, height * 0.7, 0, 0, 0.3);
+        balls = new ArrayList<Ball>();
+        balls.add(new Ball(width / 3, height * 0.9, 0, 0, 0.2));
     }
 
     void step(double deltaT) {
         // TODO this method implements one step of simulation with a step deltaT
         for (Ball b : balls) {
-            // detect collision with the border
-            if (b.x < b.radius || b.x > areaWidth - b.radius) {
-                b.vx *= -1; // change direction of ball
+            //
+            if (b.x < b.radius) {
+                if (b.vx < 0)
+                    b.vx *= -1;
             }
-            if (b.y < b.radius || b.y > areaHeight - b.radius) {
-                b.vy *= -1;
+            if (b.x > areaWidth - b.radius) {
+                if (b.vx > 0)
+                    b.vx *= -1;
+            }
+            if (b.y < b.radius) {
+                if (b.vy < 0)
+                    b.vy *= -1;
+            }
+            if (b.y > areaHeight - b.radius) {
+                if (b.vy > 0)
+                    b.vy *= -1;
             }
 
-            b.vy-=GRAVITY;
 
+            b.vy = b.vy - GRAVITY * deltaT;
             // compute new position according to the speed of the ball
-            b.x += deltaT * b.vx;
-            b.y += deltaT * b.vy;
+            b.x = b.x + deltaT * b.vx;
+            b.y = b.y + deltaT * b.vy;
+
         }
     }
 
@@ -47,36 +59,33 @@ class Model {
      * Simple inner class describing balls.
      */
     class Ball {
+        double x, y, vx, vy, radius;
 
-        Ball(double x, double y, double vx, double vy, double r) {
+        public Ball(double x, double y, double vx, double vy, double radius) {
             this.x = x;
             this.y = y;
             this.vx = vx;
             this.vy = vy;
-            this.radius = r;
+            this.radius = radius;
         }
 
-        /**
-         * Position, speed, and radius of the ball. You may wish to add other attributes.
-         */
-        double x, y, vx, vy, radius;
-
-        public double potentialEnergy() {
+        double potentialEnergy() {
             // calculate the potential energy E = mgh
             return mass() * GRAVITY * y;
         }
 
-        public double kineticEnergy() {
+        double kineticEnergy() {
             // calculate the kinetic energy E = mv/2
-            return 1.0 / 2.0 * mass() * Math.pow(velocity(),2);
+            return (1.0 / 2.0) * mass() * Math.pow(velocity(), 2);
         }
 
-        private double mass() {
+        double mass() {
+            return 1;
             // calculate the mass (volume of sphere: 4/3*pi*r^3
-            return 4.0 / 3.0 * Math.PI * Math.pow(radius, 3);
+            // return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
         }
 
-        public double velocity() {
+        double velocity() {
             return Math.sqrt((vx * vx) + (vy * vy));
         }
 
